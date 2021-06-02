329097a13b9c8f5edfd7404f0416638f0103d794
thegisexpert <vene@usa.com>
2021-06-02 19:36:03 +0200

    version example

---
+++ example/getrecords_ajax.php
@@ -0,0 +1,38 @@
+<?php 
+    
+
+
+
+    $mysql_host= 'localhost:3307';
+     $mysql_database='participant';
+     $mysql_user="root";
+     $mysql_password="123456";
+
+    $result_array = array();
+    $conn = mysqli_connect($mysql_host, $mysql_user,$mysql_password,$mysql_database);
+
+
+    /* Check connection  */
+    if ($conn->connect_error) {
+         die("Connection to database failed: " . $conn->connect_error);
+    }
+    /* SQL query to get results from database */
+
+    $sql = "select * from event, participant, participant_event";
+    
+    //$sql = "SELECT id, first_name, last_name, email FROM users ";
+    
+    $result = $conn->query($sql);
+    /* If there are results from database push to result array */
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            array_push($result_array, $row);
+        }
+    }
+    /* send a JSON encded array to client */
+
+    //console.log($result_array);
+    echo (json_encode($result_array));
+    
+    $conn->close();
